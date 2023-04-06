1594c698a1ab88c5c8fabe9ac88e8e821f6657dd
Anderson Araújo Santos <97120077+sydom1@users.noreply.github.com>
2023-04-06 16:28:29 -0300

    Add files via upload

---
+++ 11-CalculaPoupanca/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("11 - Projeto Calculando Poupança");
+
+        double investimento = 1000.0;
+
+        //rendimento de 0.5% (0.005) ao mês
+        //mês 1
+        // investimento = investimento * 0.005;
+
+        int mes = 1;
+
+        while (mes <= 12)
+
+        {
+            investimento = investimento + investimento * 0.005;
+            Console.WriteLine("No mês " + mes + " voce tem " + investimento);
+
+           // mes = mes + 1; e a mesma coisa que o mes++
+           mes ++;
+        }
+
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+
+
+}
---
+++ 12-CalculadoraPoupanca2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("12 - Projeto Calculando Poupança");
+
+        double investimento = 1000.0;
+
+        for (int mes = 1; mes <= 12; mes++)
+
+        {
+            // investimento *= 1.005; simplificando a escrita do codigo abaixo.
+            investimento = investimento + investimento * 0.005;
+            Console.WriteLine("No mês " + mes + " voce tem " + investimento);
+
+
+        }
+
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+
+
+}
